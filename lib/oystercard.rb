3e9b55415d8ebdad6baac4c073148149280e52e5
John Dennehy <john.dennehy@gmail.com>
2019-09-24 16:38:49 +0100

    adds #touch_in, #touch_out and #in_journey? and passed rspec

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -5,6 +5,7 @@ class Oystercard
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -13,6 +14,22 @@ class Oystercard
     else
       @balance += amount
     end
+  end
+
+  def deduct(fare)
+    @balance -= fare
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -20,9 +20,34 @@ describe Oystercard do
       too_much_money = card.top_up(10)
       expect(too_much_money).to eq "top_up unsuccessful - over max balance #{described_class::MAXIMUM_BALANCE}"
     end
-
   end
 
+  describe "#deduct" do
+    it "deducts fare from the balance" do
+      card.top_up(5.0)
+      fare = 2.50
+      expect(card.deduct(fare)).to eq 2.5
+    end
+  end
 
+  describe "#in_journey?" do
+    it "returns false when card initialized" do
+      expect(card.in_journey?).to be_falsey
+    end
+  end
 
+  describe "#touch_in" do
+    it "sets @journey to true" do
+      card.touch_in
+      expect(card.in_journey?).to be_truthy
+    end
+  end
+
+  describe "#touch_out" do
+    it "sets @journey to true" do
+      card.touch_in
+      card.touch_out
+      expect(card.in_journey?).to be_falsey
+    end
+  end
 end
