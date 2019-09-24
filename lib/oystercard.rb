bdd9e98a0cc2442e60e3c43600a2958ce818a744
John Dennehy <john.dennehy@gmail.com>
2019-09-24 14:29:58 +0100

    adds user_stories_spec.rb

---
+++ lib/oystercard.rb
@@ -0,0 +1,5 @@
+class Oystercard
+  def add_amount
+
+  end
+end
---
+++ spec/feature_tests/user_stories_spec.rb
@@ -0,0 +1,57 @@
+
+describe "User Stories" do
+  # In order to use public transport
+  # As a customer
+  # I want money on my card
+  it " has money on the card" do
+    my_card = Oystercard.new
+    amount = 20
+    my_card.add_amount(amount)
+    expect { my_card.balance }.to eq amount
+  end
+
+  # In order to keep using public transport
+  # As a customer
+  # I want to add money to my card
+
+  # In order to protect my money
+  # As a customer
+  # I don't want to put too much money on my card
+
+  # In order to pay for my journey
+  # As a customer
+  # I need my fare deducted from my card
+
+  # In order to get through the barriers
+  # As a customer
+  # I need to touch in and out
+
+  # In order to pay for my journey
+  # As a customer
+  # I need to have the minimum amount for a single journey
+
+  # In order to pay for my journey
+  # As a customer
+  # I need to pay for my journey when it's complete
+
+  # In order to pay for my journey
+  # As a customer
+  # I need to know where I've travelled from
+
+  # In order to know where I have been
+  # As a customer
+  # I want to see to all my previous trips
+
+  # In order to know how far I have travelled
+  # As a customer
+  # I want to know what zone a station is in
+
+  # In order to be charged correctly
+  # As a customer
+  # I need a penalty charge deducted if I fail to touch in or out
+
+  # In order to be charged the correct amount
+  # As a customer
+  # I need to have the correct fare calculated
+
+end
